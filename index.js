132f58723cde2f2be2a89ef6a8ed637ac856ee07
Jason <jason13.deng@gmail.com>
2026-02-06 11:52:55 +0900

    Setup structure for password validation

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ const passwordConfirmInput = document.querySelector(".passwordConfirm");
 
 const emailInput = document.querySelector(".email");
 
-
-import { isValidEmail, validateEmail } from './Validations/emailValidation.js';
-import { isValidPostal, validatePostal } from './Validations/postalValidation.js';
-import { disableErrorMsg, enableErrorMsg } from './errorMsg.js';
-
+import { isValidEmail, validateEmail } from "./Validations/emailValidation.js";
+import {
+  isValidPostal,
+  validatePostal,
+} from "./Validations/postalValidation.js";
+import { disableErrorMsg, enableErrorMsg } from "./errorMsg.js";
+import {
+    isValidPassword,
+  isValidPasswordConfirm,
+  validatePassword,
+  validatePasswordConfirm,
+} from "./passwordValidation.js";
 
 const inputList = [
   emailInput,
@@ -18,41 +25,44 @@ const inputList = [
   passwordConfirmInput,
 ];
 
-
 const validate = (input) => {
-    if (input.id == 'email'){
-        validateEmail();
-    } else if (input.id == 'postal'){
-        validatePostal();
-    }
-}
+  if (input.id == "email") {
+    validateEmail();
+  } else if (input.id == "postal") {
+    validatePostal();
+  } else if (input.id == "password") {
+    validatePassword();
+  } else if (input.id == "passwordConfirm") {
+    validatePasswordConfirm();
+  }
+};
 
 // validation on leaving input field
 inputList.forEach((input) => {
-    input.addEventListener('focusout', function() {
-        validate(input)
-    })
-})
+  input.addEventListener("focusout", function () {
+    validate(input);
+    disableErrorMsg(input);
+  });
+});
 
 // validation on entering
 inputList.forEach((input) => {
-    input.addEventListener('focus', function() {
-        validate(input)
-    })
-})
+  input.addEventListener("focus", function () {
+    validate(input);
+  });
+});
 
 // validation on form submit
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   inputList.forEach((input) => {
+    validate(input);
     if (input.validity.valueMissing) {
       enableErrorMsg(input, "Missing value");
-    } 
+    }
   });
   console.log(`email valid : ${isValidEmail(emailInput)}`);
   console.log(`postal valid : ${isValidPostal(postalInput)}`);
-
-
+  console.log(`password valid : ${isValidPassword(passwordInput)}`);
+  console.log(`password confirm valid : ${isValidPasswordConfirm(passwordConfirmInput)}`);
 });
-
-
---
+++ passwordValidation.js
@@ -0,0 +1,27 @@
+
+import { disableErrorMsg, enableErrorMsg } from "../errorMsg.js";
+const passwordInput = document.querySelector(".password");
+const passwordConfirmInput = document.querySelector(".passwordConfirm");
+
+
+
+const validatePassword = () => {
+
+}
+
+const validatePasswordConfirm = () => {};
+
+
+const isValidPassword = (passwordInput) => {
+
+}
+
+const isValidPasswordConfirm = (passwordConfirmInput) => {
+    
+}
+
+export {validatePassword, validatePasswordConfirm, isValidPassword, isValidPasswordConfirm}
+
+
+
+
